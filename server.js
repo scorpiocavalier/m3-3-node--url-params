0a9da2802c4596d2adb21cecb3b98e77f816a1ee
Arturo Chen <art.can.fly@gmail.com>
2020-07-06 20:16:14 -0400

    1.7 done

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-'use strict';
+'use strict'
 
 const express = require('express')
-const morgan = require('morgan');
-const { top50 } = require('./data/top50');
+const morgan = require('morgan')
+const { top50 } = require('./data/top50')
 const handlers = require('./public/js/handlers')
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000
 
-const app = express();
+const app = express()
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-app.set('view engine', 'ejs');
+app.use(morgan('dev'))
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+app.set('view engine', 'ejs')
 
 // endpoints here
 app.get('/top50', (req, res) => {
@@ -33,11 +33,19 @@ app.get('/top50/popular-artist', (req, res) => {
 
 app.get('/top50/song/:number', (req, res) => {
     const number = req.params.number
-    res.render('pages/singleSong', { 
-        title: `Song #${number}`,
-        songs: top50,
-        number 
-    })
+    if(number > 0 && number <= 50)
+        res.status(200)
+            .render('pages/singleSong', { 
+                title: `Song #${number}`,
+                songs: top50,
+                number
+            })
+    else
+        res.status(404)
+            .render('pages/fourOhFour', {
+                title: 'I got nothing',
+                path: req.originalUrl
+            })
 })
 
 app.get('/', (req, res) => {
@@ -46,11 +54,11 @@ app.get('/', (req, res) => {
 
 // handle 404s
 app.get('*', (req, res) => {
-    res.status(404);
-    res.render('pages/fourOhFour', {
-        title: 'I got nothing',
-        path: req.originalUrl
-    });
-});
+    res.status(404)
+        .render('pages/fourOhFour', {
+            title: 'I got nothing',
+            path: req.originalUrl
+        })
+})
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
